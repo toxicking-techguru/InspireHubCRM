b2e424691055394f03c43e6c931a0a8df1ded267
Toxicking Techguru <bugbountyofficiall@gmail.com>
2026-04-24 03:26:58 +0000

    Toxicking
    use this config firebase
    
    // Import the functions you need fr

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDCvz2-0K2XScKIyjagLiP_IMPpgh5j1VQ",
+  authDomain: "nexuscrm-98ae2.firebaseapp.com",
+  projectId: "nexuscrm-98ae2",
+  storageBucket: "nexuscrm-98ae2.firebasestorage.app",
+  messagingSenderId: "300724125214",
+  appId: "1:300724125214:web:9ad17fea9671f025aab80f"
+};
